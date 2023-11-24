2bdc1681e0f28b02cdfcb9e1efa4afeafb2fa7d1
taek2222 <oot309@daum.net>
2023-11-24 23:28:50 +0900

    [Algorithm] 합병정렬, 퀵정렬 평균 실행 시간 분석 프로그램 완료

--- Report 2/src/problem_1/Merge_Quick.java
+++ Report 2/src/problem_1/Merge_Quick.java
@@ -8,33 +8,6 @@ public class Merge_Quick {
             array[i] = (int)(Math.random()*100); // Math 메소드를 활용해 100 미만 랜덤 수 생성.
     }
 
-    // 정렬 전, 후 출력
-    public static void printf(int[] array, int[] sort_array, String str) {
-        System.out.println("[" + str + " 정렬 전] 가장 앞 5개 배열");
-        for(int i = 0; i < 5; i++)
-            System.out.println((i + 1) + " 번째 숫자 : [" + array[i] + "] ");
-
-        System.out.println();
-
-        System.out.println("[" + str + " 정렬 전] 가장 뒤 5개 배열");
-        for(int i = array.length - 1; i > array.length-6; i--)
-            System.out.println((i + 1) + " 번째 숫자 : [" + array[i] + "] ");
-
-        System.out.println();
-
-        System.out.println("[" + str + " 정렬 후] 가장 앞 5개 배열");
-        for(int i = 0; i < 5; i++)
-            System.out.println((i + 1) + " 번째 숫자 : [" + sort_array[i] + "] ");
-
-        System.out.println();
-
-        System.out.println("[" + str + " 정렬 후] 가장 뒤 5개 배열");
-        for(int i = array.length - 1; i > array.length-6; i--)
-            System.out.println((i + 1) + " 번째 숫자 : [" + sort_array[i] + "] ");
-
-        System.out.println();
-    }
-
     // 합병 정렬(분할 정복) 분할
     public static void merge_sort_DC(int[] list, int low, int high) {
         int middle;
@@ -71,51 +44,66 @@ public class Merge_Quick {
         }
     }
     // 퀵 정렬(분할 정복) 교환
-    private static int partition(int[] list, int low, int high) {
-        int pivot = list[low];
+    public static int partition(int[] list, int low, int high) {
+        int pivot = list[low]; // 피벗을 첫 번째 요소로 선택
         int i, j = low;
-        for(i = low+1; i <= high; i++)
+        for(i = low+1; i <= high; i++) // 피벗보다 작은 요소 왼쪽, 큰 요소 오른쪽 이동
             if(list[i] < pivot) {
                 j++;
                 int temp = list[i];
                 list[i] = list[j];
                 list[j] = temp;
             }
+        // 피벗을 올바른 위치로 이동
         list[low] = list[j];
         list[j] = pivot;
-        return j;
+        return j; // 피벗 최종 위치 반환
     }
 
 
     public static void main(String[] args) {
-        int[] Random = new int[100000]; // 데이터 배열
-        Random_array(Random); // 랜덤 수 선정
-        int[] Merge = Random.clone(); // 합병 정렬 배열 선언 (랜덤 수 배열 복사)
-        int[] Quick = Random.clone(); // 퀵정렬 배열 선언 (랜덤 수 배열 복사)
+        int Data_Size = 100000; // n 데이터 사이즈
+        int[][] Data_Array = new int[10][Data_Size]; // 10개 배열의 각각 무작위 1000000개의 수 저장 배열
 
+        for(int i = 0; i < 10; i++) // 10개 배열 무작위 수 생성
+            Random_array(Data_Array[i]);
+
+        System.out.println("\n*** 총 데이터 수 "+ Data_Size +"개의 합병정렬, 퀵정렬 처리 속도 비교 *** ");
 
         // 합병 정렬 시간 측정 및 시작
-        long Merge_beforeTime = System.currentTimeMillis(); // 시간 측정 시작
+        System.out.println("\n** 랜덤 배열 각각의 실행 시간 비교 및 평균 시간 [합병 정렬] **\n");
+        double Merge_Time_Total = 0; // 합병 정렬 총 걸린 시간
+        for(int i = 0; i < 10; i++) {
+            int[] Merge_Array = Data_Array[i].clone(); // 기존 데이터 보존을 위한 복사
+            long Merge_beforeTime = System.currentTimeMillis(); // 시간 측정 시작
 
-        merge_sort_DC(Merge, 0, Merge.length-1); // 합병 정렬 START
-        printf(Random, Merge, "합병");
+            merge_sort_DC(Merge_Array, 0, Merge_Array.length-1); // 합병 정렬 시작
+
+            long Merge_afterTime = System.currentTimeMillis(); // 시간 측정 완료
+            long Merge_result = Merge_afterTime - Merge_beforeTime; // 최종 시간 계산
+
+            System.out.println( (i+1) + "번째 데이터 실행 시간 : " + Merge_result); // 각각의 실행 시간 출력
+            Merge_Time_Total += Merge_result; // 실행 시간 합산
+        }
 
 
-        long Merge_afterTime = System.currentTimeMillis(); // 시간 측정 완료
-        long Merge_result = Merge_afterTime - Merge_beforeTime; // 최종 시간 계산
+        // 퀵정렬 시간 측정 및 시작
+        System.out.println("\n** 랜덤 배열 각각의 실행 시간 비교 및 평균 시간 [퀵 정렬] **\n");
+        double Quick_Time_Total = 0; // 퀵 정렬 총 걸린 시간
+        for(int i = 0; i < 10; i++) {
+            int[] Quick_Array = Data_Array[i].clone(); // 기존 데이터 보존을 위한 복사
+            long Quick_beforeTime = System.currentTimeMillis(); // 시간 측정 시작
 
-        //퀵정렬 시간 측정 및 시작
-        long Quick_beforeTime = System.currentTimeMillis(); // 시간 측정 시작
+            quicksort_DC(Quick_Array, 0, Quick_Array.length-1); // 퀵 정렬 시작
 
-        quicksort_DC(Quick, 0, Quick.length-1);
-        printf(Random, Quick, "퀵");
+            long Quick_afterTime = System.currentTimeMillis(); // 시간 측정 완료
+            long Quick_result = Quick_afterTime - Quick_beforeTime; // 최종 시간 계산
 
-        long Quick_afterTime = System.currentTimeMillis(); // 시간 측정 완료
-        long Quick_result = Quick_afterTime - Quick_beforeTime; // 최종 시간 계산
-        System.out.println("합병 정렬 실행 시간(ms) : " + Merge_result);
-        System.out.println("퀵 정렬 실행 시간(ms) : " + Quick_result);
+            System.out.println( (i+1) + "번째 데이터 실행 시간 : " + Quick_result); // 각각의 실행 시간 출력
+            Quick_Time_Total += Quick_result; // 실행 시간 합산
+        }
 
+        System.out.println("\n[합병 정렬] 평균 실행 시간 : " + Merge_Time_Total/10 + " ms"); // 합병 정렬 총 시간 출력
+        System.out.println("[퀵 정렬] 평균 실행 시간 : " + Quick_Time_Total/10 + " ms"); // 퀵 정렬 총 시간 출력
     }
 }
-
-
