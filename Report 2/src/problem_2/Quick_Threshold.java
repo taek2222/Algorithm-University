c9f4dcf4aa5a3762943834b205697af9fc656216
taek2222 <oot309@daum.net>
2023-11-24 22:54:33 +0900

    [Algorithm] 퀵정렬 임계값 완성

--- Report 2/src/problem_2/Quick_Threshold.java
+++ Report 2/src/problem_2/Quick_Threshold.java
@@ -1,5 +1,7 @@
 package problem_2;
 
+import java.util.*;
+
 public class Quick_Threshold {
     public static void Random_array(int[] array) {
         for(int i = 0; i < array.length; i++)
@@ -21,66 +23,51 @@ public class Quick_Threshold {
         }
     }
 
-    // 퀵 정렬(분할 정복) 교환
+    // 퀵 정렬(분할 정복)
     private static int partition(int[] list, int low, int high) {
-        int pivot = list[low];
+        int pivot = list[low]; // 피벗을 배열의 첫 번째 요소로 선택
         int i, j = low;
-        for(i = low+1; i <= high; i++)
+        for(i = low+1; i <= high; i++) // 피벗보다 작은 요소 왼쪽, 큰 요소 오른쪽 이동
             if(list[i] < pivot) {
                 j++;
                 int temp = list[i];
                 list[i] = list[j];
                 list[j] = temp;
             }
+        // 피벗을 올바른 위치로 이동
         list[low] = list[j];
         list[j] = pivot;
-        return j;
+        return j; // 피벗 최종 위치 반환
     }
 
     // 삽입 정렬
     public static void insertionSort(int[] list, int low, int high) {
         for (int i = low + 1; i <= high; i++) {
-            int next = list[i];
+            int next = list[i]; // 삽입할 요소
             int j;
+            // 왼쪽으로 이동하며 삽입할 위치 찾
             for (j = i - 1; j >= low && list[j] > next; j--)
                 list[j + 1] = list[j];
+            // 삽입 위치에 요소 삽입
             list[j + 1] = next;
         }
     }
 
-    // 임계값에 따른 퀵정렬
-
-
     public static void main(String[] args) {
         int Data_Size = 1000000; // n 데이터 사이즈
         int[][] Data_Array = new int[10][Data_Size]; // 10개 배열의 각각 무작위 1000000개의 수 저장 배열
-        double Time_Total;
+        List<Map.Entry<Integer, Double>> averageTimesList = new ArrayList<>();
 
         for(int i = 0; i < 10; i++) // 10개 배열 무작위 수 생성
             Random_array(Data_Array[i]);
 
-        System.out.println("\n*** 총 데이터 수 "+ Data_Size +"개의 임계값의 따른 퀵정렬 처리 속도 비교 *** \n");
-
-        // 퀵정렬 시간 측정 및 시작
-        System.out.println("** 랜덤 배열 각각의 실행 시간 비교 및 평균 시간 [일반 퀵정렬] **");
-        Time_Total = 0; // 총 시간 초기화
-        for(int i = 0; i < 10; i++) {
-            int[] Quick_Threshold = Data_Array[i].clone(); // 기존 데이터 보존을 위한 복사
-            long Quick_Threshold_beforeTime = System.currentTimeMillis(); // 일반 퀵정렬 시간 측정 시작
-
-            quicksort_DC(Quick_Threshold, 0, Data_Size-1, 0); // 퀵 정렬 실행 (임계값 0 = 퀵정렬)
-
-            long Quick_Threshold_afterTime = System.currentTimeMillis(); // 시간 측정 완료
-            long Quick_Threshold_result = Quick_Threshold_afterTime - Quick_Threshold_beforeTime; // 최종 시간 계산
-            System.out.println( (i+1) + "번째 데이터 실행 시간 : " + Quick_Threshold_result); // 각각의 실행 시간 출력
-            Time_Total += Quick_Threshold_result; // 총 실행 시간 합산
-        }
-        System.out.println("[일반 퀵정렬]의 평균 실행 시간 : " + Time_Total/Data_Array.length); // 평균 실행 시간 계산
+        System.out.println("\n*** 총 데이터 수 "+ Data_Size +"개의 임계값의 따른 퀵정렬 처리 속도 비교 *** ");
 
         // 임계값에 따른 퀵정렬 시간 측정 반복문
-        for(int Threshold = 1; Threshold <= Data_Size; Threshold *= 10) {
+        for(int Threshold = 0; Threshold <= Data_Size; Threshold = (Threshold == 0 ? 1 : Threshold * 10)) {
             System.out.println("\n** 랜덤 배열 각각의 실행 시간 비교 및 평균 시간 [임계값 : "+Threshold+"의 퀵정렬] **\n");
-            Time_Total = 0; // 총 시간 초기화
+            double Time_Total = 0; // 총 시간 초기화
+
             for(int i = 0; i < 10; i++) {
                 int[] Quick_Threshold = Data_Array[i].clone(); // 기존 데이터 보존을 위한 복사
                 long Quick_Threshold_beforeTime = System.currentTimeMillis(); // 일반 퀵정렬 시간 측정 시작
@@ -89,10 +76,22 @@ public class Quick_Threshold {
 
                 long Quick_Threshold_afterTime = System.currentTimeMillis(); // 시간 측정 완료
                 long Quick_Threshold_result = Quick_Threshold_afterTime - Quick_Threshold_beforeTime; // 최종 시간 계산
+
                 System.out.println( (i+1) + "번째 데이터 실행 시간 : " + Quick_Threshold_result); // 각각의 실행 시간 출력
                 Time_Total += Quick_Threshold_result; // 총 실행 시간 합산
             }
-            System.out.println("[임계값 : "+Threshold+"의 퀵정렬]의 평균 실행 시간 : " + Time_Total/Data_Array.length); // 평균 실행 시간 계산
+
+            double averageTime = Time_Total / 10;
+            averageTimesList.add(new AbstractMap.SimpleEntry<>(Threshold, averageTime)); // 평균 시간을 리스트에 저장
+            System.out.println("[임계값 : "+Threshold+"의 퀵정렬]의 평균 실행 시간 : " + averageTime +" ms"); // 평균 실행 시간 계산
+        }
+
+        // 평균 실행 시간을 기준으로 리스트 오름차순 정렬 및 출력
+        averageTimesList.sort(Map.Entry.comparingByValue());
+
+        System.out.println("\n평균 실행 시간에 따른 임계값 (오름차순):");
+        for(Map.Entry<Integer, Double> entry : averageTimesList) {
+            System.out.println("임계값: " + entry.getKey() + ", 평균 실행 시간: " + entry.getValue() + " ms");
         }
     }
 }
